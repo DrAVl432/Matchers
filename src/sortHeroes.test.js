7f24d3d1dd1239cc5509e36e065f0b0200d98315
DrAVl <DrAV432@mail.ru>
2024-06-18 12:14:55 +0700

    Matchers

---
+++ src/sortHeroes.test.js
@@ -0,0 +1,16 @@
+import sortHeroes from './sortHeroes';
+
+test('should sort heroes by health in descending order', () => {
+  const input = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+  const result = sortHeroes(input);
+  const expected = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+  expect(result).toEqual(expected);
+});
